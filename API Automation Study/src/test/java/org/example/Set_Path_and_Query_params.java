bfe6b70c9eeaa917a34e4bf52b1f3b1a9f122374
shreedlondhe <shreedlondhe@gmail.com>
2026-04-26 16:17:29 +0530

    added path params methods

--- API Automation Study/src/test/java/org/example/Set_Path_and_Query_params.java
+++ API Automation Study/src/test/java/org/example/Set_Path_and_Query_params.java
@@ -1,4 +1,18 @@
 package org.example;
 
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+
 public class Set_Path_and_Query_params {
+
+    @Test
+    void setPathParams(){
+        RestAssured.given()
+                .pathParam("path","objects/1")
+                .queryParam("","")
+                //no need to add query params in url path ret assured will take automatically
+                .when().get("https://api.restful-api.dev/{path}").then().log().all();
+
+    }
+
 }
